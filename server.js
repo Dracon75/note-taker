36da19fba8d2d74370cce24a6d6328f0e3de9435
Dracon75 <nanorth75@gmail.com>
2021-04-10 17:10:09 -0400

    fixing stuff

--- server.js
+++ server.js
@@ -1,69 +1,69 @@
-const fs = require ("fs");
+const fs = require("fs");
 const express = require("express");
 
 const path = require("path");
 const { allowedNodeEnvironmentFlags } = require("process");
 const app = express();
 
-const PORT = 8080;
+const PORT = process.env.PORT || 8080;
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-app.get("/api/notes", function(req, res){
-    fs.readFile("./db/db.json", "utf-8", function(err, data){
-        if(err){
+app.get("/api/notes", function (req, res) {
+    fs.readFile("./db/db.json", "utf-8", function (err, data) {
+        if (err) {
             throw err;
         }
-       const notes = JSON.parse(data);
-       console.log(notes);
-       return res.json(notes);
+        const notes = JSON.parse(data);
+        console.log(notes);
+        return res.json(notes);
     });
 })
 
-app.post("/api/notes",function(req, res){
+app.post("/api/notes", function (req, res) {
     const note = req.body;
-    fs.readFile("./db/db.json", "utf-8", function(err, data){
-        if(err){
+    fs.readFile("./db/db.json", "utf-8", function (err, data) {
+        if (err) {
             throw err;
         }
-       let notes = JSON.parse(data);
-       notes.push(note);
-       notes = JSON.stringify(notes);
-    fs.writeFile("./db/db.json", notes, function(err){
-        if(err){
-            throw err;
-        }
-    })
+        let notes = JSON.parse(data);
+        notes.push(note);
+        notes = JSON.stringify(notes);
+        fs.writeFile("./db/db.json", notes, function (err) {
+            if (err) {
+                throw err;
+            }
+        })
     });
     return res.json(note);
 })
 
 
-app.delete('/notes/:id', function(req, res) {
+app.delete('/notes/:id', function (req, res) {
     var id = req.params.id;
     app.db('notes').remove({
-      id: id
+        id: id
     });
     return res.status(201).end();
-  });
+});
 
 
-app.get("/notes", function(req, res){
+app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, '../note-taker/public/notes.html'));
 })
 
-app.get("*", function(req, res){
+app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, '../note-taker/public/index.html'));
 })
 
 app.delete('/notes', function (req, res) {
     res.send('Got a DELETE request at /notes')
-  })
-
-app.listen(PORT, function(){
-    console.log("app listening...", PORT);
 })
+
+app.listen(PORT, () => {
+    console.log(`App listening on PORT: ${PORT}`);
+});
 
